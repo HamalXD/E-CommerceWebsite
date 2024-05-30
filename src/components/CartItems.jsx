9bce6da99c130e08534f5cb078fe8e6505b52857
hamalxd <sammanhamal06@gmail.com>
2024-05-30 20:59:00 +0545

    updated cart items

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
 
 const CartItems = ({ item }) => {
   const { id, image, title, price, amount } = item;
@@ -10,6 +11,34 @@ const CartItems = ({ item }) => {
           {" "}
           <img className="max-w-[80px]" src={item.image} alt={item.title} />
         </Link>
+        <div className="w-full flex flex-col">
+          <div className="flex justify-between mb-2">
+            <Link
+              to={`/product/${id}`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              {title}
+            </Link>
+            <div className="text-xl cursor-pointer">
+              <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
+            </div>
+          </div>
+          <div className="bg-pink-200 flex gap-x-2 h-[36px] text-sm">
+            <div className="flex flex-1 max-w-[100px] bg-blue-400 items-center h-full border text-primary font-medium">
+              <div className=" flex flex-1 justify-center items-center cursor-pointer">
+                <IoMdRemove />
+              </div>
+              <div className="h-full flex justify-center items-center px-2">
+                {amount}
+              </div>
+              <div className="flex flex-1 h-full justify-center items-center cursor-pointer">
+                <IoMdAdd />
+              </div>
+            </div>
+            <div>Item Price</div>
+            <div>Total Price</div>
+          </div>
+        </div>
       </div>
     </div>
   );
